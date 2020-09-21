4e4e92faa7dd32763ab4aff9dabe4ea26c944cc0
Thales Rodrigues <thales.brito@hotmail.com>
2020-09-20 23:56:20 -0300

    Smoothing out difficulty curve

--- src/data/difficulty_levels.ts
+++ src/data/difficulty_levels.ts
@@ -18,7 +18,7 @@ export default [
         score: 3,
         rotation: {
             minSpeed: 5,
-            maxSpeed: 10,
+            maxSpeed: 5,
             minTime:  1000,
             maxTime:  10000,
             turnChance: 0.25,
@@ -33,7 +33,7 @@ export default [
         score: 6,
         rotation: {
             minSpeed: 5,
-            maxSpeed: 15,
+            maxSpeed: 10,
             minTime:  1000,
             maxTime:  10000,
             turnChance: 0.25,
@@ -47,8 +47,23 @@ export default [
     {
         score: 10,
         rotation: {
+            minSpeed: 5,
+            maxSpeed: 15,
+            minTime:  1000,
+            maxTime:  10000,
+            turnChance: 0.25,
+            acceleration: 60,
+        },
+        colors: {
+            timeout: 2500,
+            sameAsTextChance: 1/6
+        }  
+    },
+    {
+        score: 20,
+        rotation: {
             minSpeed: 10,
-            maxSpeed: 20,
+            maxSpeed: 15,
             minTime:  1000,
             maxTime:  10000,
             turnChance: 0.25,
@@ -60,13 +75,28 @@ export default [
         }  
     },
     {
-        score: 20,
+        score: 30,
         rotation: {
-            minSpeed: 15,
-            maxSpeed: 30,
+            minSpeed: 10,
+            maxSpeed: 20,
             minTime:  1000,
             maxTime:  10000,
-            turnChance: 0.25,
+            turnChance: 0.5,
+            acceleration: 60,
+        },
+        colors: {
+            timeout: 1750,
+            sameAsTextChance: 1/6
+        }  
+    },
+    {
+        score: 40,
+        rotation: {
+            minSpeed: 10,
+            maxSpeed: 25,
+            minTime:  1000,
+            maxTime:  8000,
+            turnChance: 0.5,
             acceleration: 60,
         },
         colors: {
@@ -75,12 +105,27 @@ export default [
         }  
     },
     {
-        score: 30,
+        score: 50,
         rotation: {
-            minSpeed: 20,
-            maxSpeed: 35,
+            minSpeed: 10,
+            maxSpeed: 30,
             minTime:  1000,
-            maxTime:  10000,
+            maxTime:  8000,
+            turnChance: 0.5,
+            acceleration: 60,
+        },
+        colors: {
+            timeout: 1375,
+            sameAsTextChance: 1/6
+        }  
+    },
+    {
+        score: 60,
+        rotation: {
+            minSpeed: 15,
+            maxSpeed: 30,
+            minTime:  1000,
+            maxTime:  8000,
             turnChance: 0.5,
             acceleration: 60,
         },
@@ -90,10 +135,25 @@ export default [
         }  
     },
     {
-        score: 40,
+        score: 70,
         rotation: {
             minSpeed: 20,
-            maxSpeed: 40,
+            maxSpeed: 30,
+            minTime:  1000,
+            maxTime:  8000,
+            turnChance: 0.5,
+            acceleration: 60,
+        },
+        colors: {
+            timeout: 1125,
+            sameAsTextChance: 1/6
+        }  
+    },
+    {
+        score: 80,
+        rotation: {
+            minSpeed: 20,
+            maxSpeed: 35,
             minTime:  1000,
             maxTime:  8000,
             turnChance: 0.5,
