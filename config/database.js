04c0215d786809a0f3c3f33fdb9a1c7805c7af1f
Thales Rodrigues <thales.brito@hotmail.com>
2021-08-19 21:30:47 -0300

    PostgreSQL SSL bug fix

--- config/database.js
+++ config/database.js
@@ -70,6 +70,15 @@ module.exports = {
   */
   pg: {
     client: 'pg',
-    connection: Env.get('DATABASE_URL', 'postgres://root:@localhost:5432/adonis') + "?ssl=true",
+    connection: {
+      host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT', ''),
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', 'adonis'),
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
   }
 }
